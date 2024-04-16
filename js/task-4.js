a96ec80a32311ca7e3a248ba2e0afc082b199ba7
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2024-04-16 20:14:39 +0300

    update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,49 +1,46 @@
 const getTotalBalanceByGender = (users, gender) => {
-  let total = 0;
-  users.forEach((user) => {
-    if (user.gender === gender) {
-      total += user.balance;
-    }
-  });
-  return total;
+    return users
+        .filter(user => user.gender === gender)
+        .reduce((total, user) => total + user.balance, 0);
 };
 
+
 const clients = [
-  {
+	{
     name: "Moore Hensley",
     gender: "male",
-    balance: 2811,
+    balance: 2811
   },
   {
     name: "Sharlene Bush",
     gender: "female",
-    balance: 3821,
+    balance: 3821
   },
   {
     name: "Ross Vazquez",
     gender: "male",
-    balance: 3793,
+    balance: 3793
   },
   {
     name: "Elma Head",
     gender: "female",
-    balance: 2278,
+    balance: 2278
   },
   {
     name: "Carey Barr",
     gender: "male",
-    balance: 3951,
+    balance: 3951
   },
   {
     name: "Blackburn Dotson",
     gender: "male",
-    balance: 1498,
+    balance: 1498
   },
   {
     name: "Sheree Anthony",
     gender: "female",
-    balance: 2764,
-  },
+    balance: 2764
+  }
 ];
 
 console.log(getTotalBalanceByGender(clients, "male")); // 12053
